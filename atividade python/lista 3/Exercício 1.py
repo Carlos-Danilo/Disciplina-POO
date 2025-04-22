f3e5cca35a1b6c4c2ecda87c799cb5a939955b9e
Carlos-Danilo <carlos.danilo@escolar.ifrn.edu.br>
2025-04-22 14:29:14 +0000

    lista 3

---
+++ atividade python/lista 3/Exercício 1.py
@@ -0,0 +1,11 @@
+
+v1=int(input())
+v2=int(input())
+
+PROD=v1*v2
+
+print("PROD =",PROD)
+
+
+
+
---
+++ atividade python/lista 3/Exercício 4.py
@@ -0,0 +1,3 @@
+1, pdp = map (int, input().split())
+posicao_inicial= pdp % 1
+print(posicao_inicial)
