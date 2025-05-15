62f0ff2a959118dcdc2ae68856d9b8317ed24c30
Carlos-Danilo <carlos.danilo@escolar.ifrn.edu.br>
2025-05-15 14:52:36 +0000

    revisão

---
+++ atividade python/Revisão 1°B/revisão.py
@@ -0,0 +1,40 @@
+# print(1 - 2 + 3 * 4) 
+# print(1 * 2 - 3 * 4)
+# print(1 / 2 + 3 * 4)
+# print(1 // 2 * 3 + 4) 
+# print(1 + 2 * 3 / 4)
+
+
+# II. Escreva um programa em Python para:
+# 1. ler três números inteiros em uma mesma linha
+# 2. mostrar a soma dos números pares
+# Exemplo: Digite três números inteiros separados por espaços
+# 10 15 20
+# Soma dos pares: 30
+
+# entrada = input("Digite três números inteiros separados por espaços: ")
+
+# numeros = list(map(int, entrada.split()))
+# soma_pares = 0
+# for numero in numeros:
+#     if numero % 2 == 0:
+#         soma_pares += numero
+# print(f"Soma dos pares: {soma_pares}")
+
+
+
+# III. Escreva um programa em Python para:
+# 1. ler uma frase
+# 2. mostrar a frase lida, retirando os 2o, 4o, 6o, ... caracteres, ou seja, escreve um, outro não, a partir do
+# primeiro caractere, como no exemplo:
+# Exemplo: Escreva uma frase
+# Técnico em Informática
+# Tcioe nomtc
+
+
+# print("Escreva uma frase")
+# frase = input()
+# for i in range(0, len(frase), 2):
+#     print(frase[i], end="")
+
+
