58bc500f5db88dfde6252014d413679fb8e227d8
Carlos-Danilo <carlos.danilo@escolar.ifrn.edu.br>
2025-05-29 14:59:32 +0000

    retangulo

---
+++ Aula class/Aula_10.py
@@ -0,0 +1,113 @@
+
+class retangulo:
+    def __init__(self):
+        self.__base = ""
+        self.__altura = ""
+
+    def set_base (self, base):
+        if base < 1 : raise ValueError("O número deve ser positivo")
+    def get_base (self):
+        return self.__base
+    
+    def set_altura(self, altura):
+        if altura < 1 : raise ValueError
+    def get_altura(self):
+        return self.__altura
+
+    def set_area (self):
+        return self.__base * self.__altura
+    
+    def set_diagonal (self):
+        return self.__base**2 + self.__altura**2
+
+x=retangulo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
