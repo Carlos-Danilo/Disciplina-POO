9fc7d834daddd0ee0450f6c4dcd5185056c85f30
Carlos-Danilo <carlos.danilo@escolar.ifrn.edu.br>
2025-07-01 12:00:05 +0000

    q1

--- Lista 4.py
+++ Lista 4.py
@@ -17,3 +17,66 @@ class Bingo:
 
     def get_sorteados(self):
         return self.sorteados
+
+class BingoUI:
+    def __init__(self):
+        self.jogo = None
+
+    def menu(self):
+        while True:
+            print("\n=== BINGO ===")
+            print("1. Novo jogo")
+            print("2. Sortear número")
+            print("3. Ver sorteados")
+            print("4. Sair")
+            op = input("Escolha: ")
+
+            if op == "1":
+                self.novo_jogo()
+            elif op == "2":
+                self.sortear()
+            elif op == "3":
+                self.ver_sorteados()
+            elif op == "4":
+                print("Saindo do jogo...")
+                break
+            else:
+                print("Opção inválida!")
+
+    def novo_jogo(self):
+        try:
+            n = int(input("Total de bolas: "))
+            if n > 0:
+                self.jogo = Bingo(n)
+                print("Novo jogo iniciado!")
+            else:
+                print("Número deve ser maior que zero.")
+        except:
+            print("Entrada inválida.")
+
+    def sortear(self):
+        if not self.jogo:
+            print("Inicie um jogo primeiro.")
+            return
+
+        num = self.jogo.sortear()
+        if num == -1:
+            print("Todas as bolas já foram sorteadas.")
+        else:
+            print("Número sorteado:", num)
+
+    def ver_sorteados(self):
+        if not self.jogo:
+            print("Inicie um jogo primeiro.")
+            return
+
+        lista = self.jogo.get_sorteados()
+        if lista:
+            print("Sorteados:", ", ".join(map(str, lista)))
+        else:
+            print("Nenhum número foi sorteado ainda.")
+
+
+if __name__ == "__main__":
+    ui = BingoUI()
+    ui.menu()
