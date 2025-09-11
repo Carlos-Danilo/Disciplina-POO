16d0c284ea74ea7237ee9b1d9ecbd49da7082ffa
Carlos-Danilo <carlos.danilo@escolar.ifrn.edu.br>
2025-09-11 14:47:12 +0000

    streamlit

---
+++ Agenda/index.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from retanguloUI import RetanguloUI
+
+RetanguloUI.main()
+
+
+#st.header("Streamlit em Python")
+# st.button("Clique aqui"):
+#    st.write("Minha Primeira Página")
